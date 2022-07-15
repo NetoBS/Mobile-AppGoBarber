c9264eda9c652aad32c7f545a7e7f9f26b2f0adf
Neto Sousa <netosousa29@yahoo.com>
2022-07-15 17:15:39 -0300

    Melhorando a usabilidade do input SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,13 @@
 import React, { useRef } from "react";
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { 
+    Image, 
+    View, 
+    ScrollView, 
+    KeyboardAvoidingView, 
+    Platform,
+    TextInput,
+} from 'react-native';
+
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from "@react-navigation/native";
 import { Form } from "@unform/mobile";
@@ -20,6 +28,9 @@ import {
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
     const navigation = useNavigation();
+
+    const emailInputRef = useRef<TextInput>(null);
+    const passwordInputRef = useRef<TextInput>(null);
 
     return (
         <>
@@ -42,10 +53,41 @@ const SignUp: React.FC = () => {
                         <Form ref={formRef} onSubmit={(data) => {
                             console.log(data);
                         }}>
-                            <Input name="name" icon="user" placeholder="Nome" /> 
-                            <Input name="email" icon="mail" placeholder="E-mail" /> 
-                            <Input name="password" icon="lock" placeholder="Senha" />
-                        
+                            <Input
+                                autoCapitalize="words"
+                                name="name" 
+                                icon="user" 
+                                placeholder="Nome" 
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    emailInputRef.current?.focus()
+                                }}
+                            /> 
+
+                            <Input 
+                                ref={emailInputRef}
+                                keyboardType="email-address"
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                name="email" 
+                                icon="mail" 
+                                placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current?.focus()
+                                }} 
+                            /> 
+
+                            <Input 
+                                ref={passwordInputRef}
+                                secureTextEntry
+                                name="password" 
+                                icon="lock" 
+                                placeholder="Senha" 
+                                textContentType="newPassword"
+                                returnKeyType="send"
+                                onSubmitEditing={() => formRef.current?.submitForm() }
+                            />
 
                             <Button onPress={() => formRef.current?.submitForm()}>
                                 Entrar
