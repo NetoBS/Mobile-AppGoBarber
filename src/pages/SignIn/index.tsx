7eacd7406b99132ea0a065df821461d407dc3514
Neto Sousa <netosousa29@yahoo.com>
2022-07-15 16:04:09 -0300

    Melhorando a usabilidade do input SignIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,12 @@
 import React, { useCallback, useRef } from "react";
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
+import { 
+    Image, 
+    View, 
+    ScrollView, 
+    KeyboardAvoidingView, 
+    Platform,
+    TextInput
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from "@react-navigation/native";
 import { Form } from "@unform/mobile";
@@ -22,6 +29,8 @@ import {
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+    const passwordInputRef = useRef<TextInput>(null);
+    
     const navigation = useNavigation<any>();
 
     const handleSignIn = useCallback((data: object) => {
@@ -47,8 +56,29 @@ const SignIn: React.FC = () => {
                         </View>
                         
                         <Form ref={formRef} onSubmit={handleSignIn}>
-                            <Input name="email" icon="mail" placeholder="E-mail" /> 
-                            <Input name="password" icon="lock" placeholder="Senha" />
+                            <Input 
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="email-address"
+                                name="email" 
+                                icon="mail" 
+                                placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current?.focus();
+                                }}
+                            /> 
+                            <Input 
+                                ref={passwordInputRef}
+                                name="password" 
+                                icon="lock" 
+                                placeholder="Senha" 
+                                secureTextEntry
+                                returnKeyType="send"
+                                onSubmitEditing={() => {
+                                    formRef.current?.submitForm();
+                                }}
+                            />
 
                             <Button onPress={() => {
                                 formRef.current?.submitForm()
