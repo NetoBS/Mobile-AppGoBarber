bd73aa30af338c7a71d76408ec92d69a1ac75a14
Neto Sousa <netosousa29@yahoo.com>
2022-07-17 09:53:42 -0300

    Remoção de alguns arquivos desnessários e adição do async-storage

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -80,7 +80,7 @@ const SignUp: React.FC = () => {
                 'Ocorreu um erro ao fazer login, cheque as credenciais.',
             );
         }
-    }, []);
+    }, [navigation]);
 
     return (
         <>
